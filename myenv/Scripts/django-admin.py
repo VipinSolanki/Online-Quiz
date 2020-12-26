b2c3ef3568db08f98340d345edcc5d0392b4e328
VipinSolanki <70526186+VipinSolanki@users.noreply.github.com>
2020-12-26 15:36:39 +0530

    Online Quiz
    Online Quiz

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\micro\downloads\lets-quiz-master\lets-quiz-master\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
